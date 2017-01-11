192746789cca7a9e05bd65f5d64726a2e5b67a7c
vmasto <vmasto@gmail.com>
2017-01-11 03:06:43 +0200

    Improve dummy route comment

--- src/routes.js
+++ src/routes.js
@@ -24,7 +24,8 @@ routes.get('/list', (req, res, next) => {
   if (title == null || title === '') {
     // You probably want to set the response HTTP status to 400 Bad Request
     // or 422 Unprocessable Entity instead of the default 500 of
-    // the global error handler. This is just for demo purposes.
+    // the global error handler (e.g check out https://github.com/kbariotis/throw.js).
+    // This is just for demo purposes.
     next(new Error('The "title" parameter is required'));
     return;
   }
