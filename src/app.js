98ee7d3b94435bb8d49113c22e64ec7923937793
Julio J. Ticona Quispe <julioticona1508@gmail.com>
2021-05-24 11:35:55 -0500

    test

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use('/', routes);
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error('Not Found!');
   err.status = 404;
   next(err);
 });
